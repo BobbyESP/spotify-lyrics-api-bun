5f8fccf3ab638227dfd9239fa8474d67a2a9c986
Gabriel Fontán <gabilessto@gmail.com>
2025-02-01 13:29:39 +0100

    feat: use nodejs for javascript target chore: updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,54 @@
 import { Elysia } from "elysia";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
 import { lyricsController } from "./controllers/LyricsController";
+import { node } from "@elysiajs/node";
 import { ServerMessage } from "./types/ServerMessage";
 
-const app = new Elysia()
-  .use(swagger({
-    documentation: {
+const app = new Elysia({ adapter: node() })
+  .use(
+    swagger({
+      documentation: {
         info: {
-            title: 'Spotify Synced Lyrics API',
-            description: 'A simple API that retrieves the synced lyrics from the Spotify servers.',
-            version: "1.0.0",
+          title: "Spotify Synced Lyrics API",
+          description:
+            "A simple API that retrieves the synced lyrics from the Spotify servers.",
+          version: "1.0.0",
         },
         tags: [
           { name: "General", description: "General endpoints" },
-          { name : "Lyrics", description: "The lyrics fetching endpoints" }
-        ]
-    }
-}))
-  .get("/", () => {
-    return {
+          { name: "Lyrics", description: "The lyrics fetching endpoints" },
+        ],
+      },
+    })
+  )
+  .get(
+    "/",
+    () => {
+      return {
         error: false,
-        message: "This is the root of the API, no content here. " + 
-        "Try other endpoints explained in the documentation or check the Swagger page at " +
-        app.server?.hostname + ((app.server?.port ? `:${app.server?.port}` : "") + "/swagger"),
-    };
-  }, {
-    detail: {
-        tags: ["General"]
+        message:
+          "This is the root of the API, no content here. " +
+          "Try other endpoints explained in the documentation or check the Swagger page at " +
+          app.server?.hostname +
+          ((app.server?.port ? `:${app.server?.port}` : "") + "/swagger"),
+      };
+    },
+    {
+      detail: {
+        tags: ["General"],
+      },
     }
-  })
+  )
   .onError((context) => {
     return {
       error: true,
       message: context.error.message,
-  }; 
+    };
   })
   .use(lyricsController)
+  .onAfterResponse(() => {
+    console.log("Response", performance.now());
+  })
   .listen(process.env.PORT ?? 3000);
 
 console.log(
